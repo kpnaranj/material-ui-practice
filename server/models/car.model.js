ca78470c25a007ba820ef71be4500cd17bfd28c5
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-11-21 15:42:15 -0500

    Schema car created

---
+++ server/models/car.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const CarSchema = new mongoose.Schema(
+  {
+    model: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 100,
+      unique: false,
+      index: true,
+    },
+    make: {
+      type: String,
+      unique: false,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      index: true,
+    },
+    year: {
+      type: number,
+      trim: true,
+      required: true,
+      max: 4,
+    },
+    fuelType: {
+      type: String,
+      trim: true,
+      max: 100,
+      required: true,
+      unique: false,
+      lowercase: true,
+    },
+    details: {
+      type: String,
+      required: true,
+    },
+    kilometers: {
+      type: Number,
+      default: null,
+    },
+    price: {
+      type: Number,
+      default: null,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamp: true }
+);
+
+
+const Car = mongoose.model("Car", CarSchema);
+export default Car;
